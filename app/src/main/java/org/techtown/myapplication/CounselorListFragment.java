4debb0f18ab6d70616a56b4af3fee445b5f87732
kangsaan <lhs020627@naver.com>
2023-12-08 14:22:24 +0900

    수레바퀴(설정) 버튼 추가

--- app/src/main/java/org/techtown/myapplication/CounselorListFragment.java
+++ app/src/main/java/org/techtown/myapplication/CounselorListFragment.java
@@ -1,5 +1,6 @@
 package org.techtown.myapplication;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
@@ -7,6 +8,7 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -56,8 +58,16 @@ public class CounselorListFragment extends Fragment {
     }
 
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_counselor, container, false);
+        Button btn5 = view.findViewById(R.id.gotoSetting4);
+        btn5.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getActivity(), Setting.class);
+                startActivity(intent);
+            }
+        });
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_counselorlist, container, false);
     }
--- app/src/main/java/org/techtown/myapplication/MedicalrecordsFragment.java
+++ app/src/main/java/org/techtown/myapplication/MedicalrecordsFragment.java
@@ -1,5 +1,6 @@
 package org.techtown.myapplication;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
@@ -7,6 +8,7 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -56,8 +58,18 @@ public class MedicalrecordsFragment extends Fragment {
     }
 
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_counselor, container, false);
+        Button btn6 = view.findViewById(R.id.gotoSetting4);
+
+        btn6.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getActivity(), Setting.class);
+                startActivity(intent);
+            }
+        });
+
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_medicalrecords, container, false);
     }
